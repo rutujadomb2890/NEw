925d00442dfbd9adf153d6b1110bc3fd15642989
Rutuja Domb <rutujadomb2890@gmail.com>
2026-03-11 21:46:47 +0530

    commit

--- ServerSide/routes/enrolllementRoutes.js
+++ ServerSide/routes/enrolllementRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { enrollCourse, getEnrolledCourses, checkEnrollment, markLessonCompleted } = require("../controllers/enrollementController");
 
-
+// POST routes first (before parametrized routes)
 router.post("/enrollments", enrollCourse);
 router.post("/enrollments/mark-lesson-completed", markLessonCompleted);
+
+// GET routes with parameters
 router.get("/enrollments/:studentId/:courseId", checkEnrollment);
 router.get("/enrollments/:studentId", getEnrolledCourses);
 
